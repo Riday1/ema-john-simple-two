b0713a4c2e471c22f32280cf9a459532c47ec2d1
Riday1 <rrriday100@gmail.com>
2022-11-19 18:26:12 +0600

    product component is created

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import './Product.css'
 const Product = (props) => {
-    const { img, name, id, seller, ratings } = props.product;
-    console.log(props)
+    const { handleAddToClick, product } = props;
+    const { img, name, price, seller, ratings } = product;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p className='product-price'>Price : $ {price}</p>
+                <p><small>Manufacturer : {seller}</small></p>
+                <p><small>Ratings : {ratings}</small></p>
+
+            </div>
+            <button onClick={() => handleAddToClick(product)} className='btn-cart'>
+                <p className='btn-text'>Add To Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,6 +8,9 @@ const Shop = () => {
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+    const handleAddToClick = (product) => {
+        console.log(product.name);
+    }
     return (
         <div className='shop-container'>
             <div className="product-container">
@@ -15,11 +18,12 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
+                        handleAddToClick={handleAddToClick}
                     ></Product>)
                 }
             </div>
             <div className="cart-container">
-                <h2>Order Summary</h2>
+                <h2 className='order-title'> Order Summary</h2>
             </div>
         </div>
     );
